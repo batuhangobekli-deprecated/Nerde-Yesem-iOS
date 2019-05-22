5e4a811c702dde60c43e28866e7a62b14d29dc32
batuhangobekli <btubatuhan@gmail.com>
2019-05-22 16:31:49 +0300

    Reachability created.Categories controller + nearbyrestaurantviewcontroller created

---
+++ Nerde Yesem/Nerde Yesem/Controllers/CategoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesViewController.swift
+//  Nerde Yesem
+//
+//  Created by Batuhan Göbekli on 22.05.2019.
+//  Copyright © 2019 Batuhan Göbekli. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- Nerde Yesem/Nerde Yesem/Controllers/NearbyRestaurantsViewController.swift
+++ Nerde Yesem/Nerde Yesem/Controllers/NearbyRestaurantsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NearbyRestaurantsViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
